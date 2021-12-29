2beeceb8162f869814d8815e993bd0b2aab72aca
Shah Alom <shahalom.amin@gmail.com>
2021-12-29 21:24:18 +0600

    Add option to enable maintenance mode using htaccess

---
+++ maintenance.php
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <title>Website is under maintenance</title>
+    </head>
+    <body>
+        <p style="text-align: center; margin-top: 50px;"><b>Website is under maintenance, We will be back shortly!</b></p>
+    </body>
+</html>
