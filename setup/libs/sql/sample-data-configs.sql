2796ee1de3cf4c482518557828422764ff559de2
Shah Alom <shahalom.amin@gmail.com>
2023-12-09 18:41:25 +0600

    Delete Admin config: Use_New_Story_Layout.
    Changes to be committed:
    	modified:   setup/libs/sql/sample-data-configs.sql

--- setup/libs/sql/sample-data-configs.sql
+++ setup/libs/sql/sample-data-configs.sql
@@ -42,7 +42,6 @@ INSERT INTO `---table_config---` (`var_page`, `var_name`, `var_value`, `var_defa
 ('Avatars', 'Avatar_Large', '100', '100', 'number', 'Large Avatar Size', 'Size of the large avatars in pixels (both width and height). Commonly used on the profile page.', 'define', NULL),
 ('Avatars', 'Avatar_Small', '32', '32', 'number', 'Small Avatar Size', 'Size of the small avatar in pixels (both width and height). Commonly used in the comments page.', 'define', NULL),
 ('Template', 'the_template', 'aowal', 'aowal', 'Text', 'Template', 'Default Template', 'option', ''''),
-('Template', 'Use_New_Story_Layout', 'default', 'default', 'Text', 'Use the Story layout?', 'If you want to use any additional custom Story layout, set here.', 'define', ''),
 ('Plugins', 'plugins_installed', 'snippets', '', 'text', 'Installed Plugins', 'CSV list of installed plugins.', 'option', NULL),
 ('Misc', '_time_story_totals_updated', '0', '0', 'text', 'Last updated time', 'When is the total of stories updated by status', 'option', NULL),
 ('Misc', '_story_status_published_total', '0', '0', 'text', 'Total published stories', 'Number of stories published', 'option', NULL),
